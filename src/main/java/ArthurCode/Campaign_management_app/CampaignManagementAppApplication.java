6028bb4006d7916fb31e3e0d168a77f16d04c1d9
Arthur <arturb13@protonmail.com>
2025-04-13 02:32:48 +0200

    Initial commit

---
+++ src/main/java/ArthurCode/Campaign_management_app/CampaignManagementAppApplication.java
@@ -0,0 +1,13 @@
+package ArthurCode.Campaign_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManagementAppApplication.class, args);
+	}
+
+}
